6b58d6796ad9f52b9591ea5a0187807b308cb413
myrfy001 <11474381+myrfy001@users.noreply.github.com>
2023-03-28 03:02:59 +0000

    shown in ip link, make code order link C version

--- consts.rs
+++ consts.rs
@@ -3,6 +3,7 @@ pub(crate) const RX_RING_SIZE:usize = 256;
 pub(crate) const TX_RING_SIZE:usize = 256;
 pub(crate) const RXTX_SINGLE_RING_BLOCK_SIZE:usize = 8192;
 
+pub(crate) const MAC_HWADDR: [u8; 6] = [0x52, 0x54, 0x00, 0x12, 0x34, 0x55];
 
 pub(crate) const E1000_VENDER_ID:u32 = 0x8086;
 pub(crate) const E1000_DEVICE_ID:u32 = 0x100E;
--- r4l_e1000_demo.rs
+++ r4l_e1000_demo.rs
@@ -25,7 +25,16 @@ module! {
 
 struct NetDevicePrvData {
     dev: Arc<device::Device>,
+
+    napi: Arc<net::Napi>,
+    hw_addr: Arc<pci::MappedResource>,
+    irq: u32,
 }
+
+
+// TODO not sure why it is safe to do this.
+unsafe impl Send for NetDevicePrvData {}
+unsafe impl Sync for NetDevicePrvData {}
 
 struct NetDevice {}
 
@@ -87,33 +96,66 @@ impl net::DeviceOperations for NetDevice {
     fn open(_dev: &net::Device, data: <Self::Data as kernel::PointerWrapper>::Borrowed<'_>) -> Result {
         pr_info!("Rust for linux e1000 driver demo (net device open)\n");
 
-        // init dma area
-        
+        // init dma memory for tx and rx
         Self::setup_tx_resource(data)?;
         Self::setup_rx_resource(data)?;
 
-        todo!()
+        let _ = data.irq;
+        let _ = data.hw_addr;
+
+
+        Ok(())
     }
 
     fn stop(_dev: &net::Device, _data: <Self::Data as kernel::PointerWrapper>::Borrowed<'_>) -> Result {
         pr_info!("Rust for linux e1000 driver demo (net device stop)\n");
-        todo!()
+        Ok(())
     }
 
     fn start_xmit(_skb: &net::SkBuff, _dev: &net::Device, _data: <Self::Data as kernel::PointerWrapper>::Borrowed<'_>,) -> net::NetdevTx {
         pr_info!("Rust for linux e1000 driver demo (net device start_xmit)\n");
-        todo!()
+        net::NetdevTx::Ok
+    }
+
+    fn get_stats64(_netdev: &net::Device, _data: &NetDevicePrvData, stats: &mut net::RtnlLinkStats64) {
+        pr_info!("Rust for linux e1000 driver demo (net device get_stats64)\n");
+        
+        // stats.set_rx_bytes(data.stats.rx_bytes.load(Ordering::Relaxed));
+        // stats.set_rx_packets(data.stats.rx_packets.load(Ordering::Relaxed));
+        // stats.set_tx_bytes(data.stats.tx_bytes.load(Ordering::Relaxed));
+        // stats.set_tx_packets(data.stats.tx_packets.load(Ordering::Relaxed));
+
+        stats.set_rx_bytes(0);
+        stats.set_rx_packets(0);
+        stats.set_tx_bytes(0);
+        stats.set_tx_packets(0);
     }
 }
 
 /// the private data for the adapter
 struct E1000DrvPrvData {
-
+    _netdev_reg: net::Registration<NetDevice>,
 }
 
 impl driver::DeviceRemoval for E1000DrvPrvData {
     fn device_remove(&self) {
         pr_info!("Rust for linux e1000 driver demo (device_remove)\n");
+    }
+}
+
+struct NAPI{}
+
+impl net::NapiPoller for NAPI {
+    type Data = Box<NetDevicePrvData>;
+
+    fn poll(
+        _napi: &net::Napi,
+        _budget: i32,
+        _dev: &net::Device,
+        _data: &NetDevicePrvData,
+    ) -> i32 {
+        let _ = _data.napi;
+        todo!()
     }
 }
 
@@ -129,58 +171,99 @@ impl pci::Driver for E1000Drv {
     ]}
 
 
-    fn probe(dev: &mut pci::Device, _id: core::option::Option<&Self::IdInfo>) -> Result<Self::Data> {
-        pr_info!("Rust for linux e1000 driver demo (probe)\n");
+    fn probe(dev: &mut pci::Device, id: core::option::Option<&Self::IdInfo>) -> Result<Self::Data> {
+        pr_info!("Rust for linux e1000 driver demo (probe): {:?}\n", id);
 
-
-        dev.enable_device()?;
         
         // this works like a filter, the PCI device may have up to 6 bars, those bars have different types,
         // some of them are mmio, others are io-port based. The params to the following function is a 
         // filter condition, and the return value is a mask indicating which of those bars are selected.
         let bars = dev.select_bars(bindings::IORESOURCE_MEM as u64);
 
+        // the underlying will call `pci_enable_device()`, but the C version use `pci_enable_device_mem()`
+        // TODO: find the difference between the two
+        dev.enable_device()?;
+
         // ask the os to reserve the memory region of the selected bars.
         dev.request_selected_regions(bars, c_str!("e1000 reserved memory"))?;
 
-        // get first resource
+        // set device to master mode.
+        dev.set_master();
+
+        // get resource(memory range) provided by BAR0
         let res = dev.iter_resource().nth(0).ok_or(kernel::error::code::EIO)?;
 
-        // map device registers' hardware address to logical address so the kernel driver can access it.
-        let reg_addr = dev.map_resource(&res, res.len())?;
+        // TODO pci_save_state(pdev); not supported by crate now, only have raw C bindings.
 
-        // test read the register, this test comes from https://pdos.csail.mit.edu/6.828/2011/labs/lab6/ Exercise 4.
-        let v = reg_addr.readl(0x0008)?;
-        pr_info!("read result: {}", v);
+        // alloc new ethernet device, this line represent the `alloc_etherdev()` and `SET_NETDEV_DEV()` in C version.
+        let mut netdev_reg = net::Registration::<NetDevice>::try_new(dev)?;
+        let netdev = netdev_reg.dev_get();
+
+        // map device registers' hardware address to logical address so the kernel driver can access it.
+        let hw_addr = Arc::try_new(dev.map_resource(&res, res.len())?)?;
+
+        // TODO implement C version `e1000_init_hw_struct()`
+
+        // only pci-x need 64-bit, to simplify code, hardcode 32-bit for now.
+        dma::set_coherent_mask(dev, 0xFFFFFFFF)?;
+
+        // TODO ethtool support here.
+
+        // Enable napi, the R4L will call `netif_napi_add_weight()`, the origin C version calls `netif_napi_add`
+        let napi = net::NapiAdapter::<NAPI>::add_weight(&netdev, 64)?;
+
+
+        // TODO implement C version `e1000_sw_init()`
+
+        // TODO a lot of feature flags are assigned here in the C code, skip them for now.
+
+
+
+
+
+        let irq = dev.irq();
+
+        
+
+
+        
+        
+        netdev.eth_hw_addr_set(&MAC_HWADDR);
 
 
         let common_dev = device::Device::from_dev(dev);
 
-        let mut netdev: net::Registration<NetDevice> = net::Registration::<NetDevice>::try_new(dev)?;
-        netdev.register(Box::try_new(
+        
+        netdev.netif_carrier_off();
+
+        
+        netdev_reg.register(Box::try_new(
             NetDevicePrvData {
                 dev: Arc::try_new(common_dev)?,
+                hw_addr: Arc::clone(&hw_addr),
+                napi: napi.into(),
+                irq,
             }
         )?)?;
 
-        dev.set_master();
+        
 
+        
 
-        dma::set_coherent_mask(dev, 0xFFFFFFFF)?;
-
-        // TODO pci_save_state(pdev); not supported by crate now, only have raw C bindings.
-
-        // TODO alloc_etherdev
         
         
 
         Ok(Box::try_new(
-            E1000DrvPrvData{}
+            E1000DrvPrvData{
+                // Must hold this registration, or the device will be removed.
+                _netdev_reg: netdev_reg,
+            }
         )?)
     }
 
-    fn remove(_data: &Self::Data) {
+    fn remove(data: &Self::Data) {
         pr_info!("Rust for linux e1000 driver demo (remove)\n");
+        drop(data);
     }
 }
 struct E1000KernelMod {
